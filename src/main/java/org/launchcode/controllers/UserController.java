e9912c81496376a9dbf3bb3338ecdef439317c2f
rastylus <ryan.p.riggs@gmail.com>
2023-10-04 07:13:23 -0500

    added serviceSelection.html selection link to user page

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,34 @@
+package org.launchcode.controllers;
+
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @GetMapping
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm (Model model, @ModelAttribute User user,
+                                      String verify) {
+        if (user.getPassword().equals(verify)) {
+            return "/user/index";
+        } else {
+            model.addAttribute("error", "Password must match");
+//            model.addAttribute("username", User.getUsername(username));
+            model.addAttribute(user.getEmail());
+            return "user/add";
+        }
+
+    }
+
+}
