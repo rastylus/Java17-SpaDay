13844bd58627ff5fc29e091d3c591ff9ed823869
rastylus <ryan.p.riggs@gmail.com>
2023-10-07 06:40:46 -0500

    added annotations to user model

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,17 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Must enter username.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+    @Email(message = "Not a valid email.")
     private String email;
+    @NotBlank(message = "Must enter password.")
+    @Size(min =6, message = "Password must be at least 6 characters long.")
     private String password;
 
     public User() {
